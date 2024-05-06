fd2820ab7f6b8efef483310488d61b9add38ddee
iperezgamiz <144547977+iperezgamiz@users.noreply.github.com>
2024-05-06 01:39:55 -0400

    Update client.py

--- client.py
+++ client.py
@@ -14,6 +14,7 @@ class Client:
         self.client_port = client_port
         self.stop_listening = False
         self.db_file = f'p2p_messaging_app_{username}.db'
+        self.current_chat = None
 
     def get_local_ip(self):
         """Get the local IP address of the client."""
@@ -49,56 +50,19 @@ class Client:
                     sender_username = message_info.get('sender_username')
                     message_text = message_info.get('message_text')
 
-                    if self.is_contact_blocked(sender_username):
-                        continue
-
-                    print(f"{sender_username}: {message_text}")
-
-                    db_conn = sqlite3.connect(self.db_file)
-                    cursor = db_conn.cursor()
-                    if not self.is_contact(sender_username, cursor):
-                        print(f"Do you want to accept messages from {sender_username}? [y/n]: ")
-                        user_decision = input()
-
-                        if user_decision == 'y':
-                            blocked = 0
-                        elif user_decision == 'n':
-                            blocked = 1
-                            print(f"{sender_username} has been blocked.")
-
-                        try:
-                            cursor.execute("INSERT INTO Contacts (Username, Blocked) VALUES (?, ?)",
-                                           (sender_username, blocked))
-                            db_conn.commit()
-                            if blocked == 0:
-                                print(f"Added {sender_username} to contacts.")
-                            else:
-                                continue
-                        except sqlite3.IntegrityError as e:
-                            print(f"Could not add {sender_username} to contacts: {e}")
+                    if self.current_chat == sender_username:
+                        print(f"\n{sender_username}: {message_text}")
 
                     self.store_received_message(sender_username, self.username, message_text)
+
                 except json.JSONDecodeError as e:
                     print(f"Failed to decode message: {e}")
                     continue
 
-    def is_contact_blocked(self, username):
-        """Check if the username is blocked."""
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-        cursor.execute("SELECT Blocked FROM Contacts WHERE Username = ?", (username,))
-        result = cursor.fetchone()
-        return result and result[0] == 1
-
-    def is_contact(self, username, cursor):
-        """Check if the username is already a contact."""
-        cursor.execute("SELECT Username FROM Contacts WHERE Username = ?", (username,))
-        return cursor.fetchone() is not None
-
     def store_received_message(self, sender_username, receiver_username, message_text):
         """Stores a received message in the database."""
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        delivered = 1  # Assuming the message is considered delivered upon receipt
+        delivered = 1
 
         conn = sqlite3.connect(self.db_file)
         cursor = conn.cursor()
@@ -108,21 +72,6 @@ class Client:
         ''', (sender_username, receiver_username, message_text, delivered, timestamp))
         conn.commit()
 
-    def send_message(self, receiver_username, ip, port, message):
-        """Sends a message to another client. Stores the message if the recipient is offline."""
-        try:
-            # Format the message and sender information as a JSON string
-            message_data = json.dumps({
-                'sender_username': self.username,
-                'message_text': message
-            }).encode('utf-8')
-
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(5)  # Timeout to avoid hanging indefinitely
-                s.connect((ip, port))
-                s.sendall(message_data)
-        except Exception as e:
-            print(e)
     def register_in_server(self):
         """Register this client with the central server."""
         data = json.dumps({
@@ -157,18 +106,20 @@ class Client:
         db_conn = sqlite3.connect(self.db_file)
         cursor = db_conn.cursor()
         try:
-            cursor.execute("INSERT INTO Contacts (Username, Blocked) VALUES (?, ?)",
-                           (receiver_username, 0))
-            db_conn.commit()
+            cursor.execute("INSERT INTO Contacts (Username) VALUES (?)",
+                           (receiver_username,))
             print(f"Added {receiver_username} to contacts.")
         except sqlite3.IntegrityError as e:
-            print(f"Could not add {receiver_username} to contacts: {e}")
+            print(f"Username already in contacts.")
+        self.retrieve_past_messages(receiver_username, db_conn, cursor)
         # Check if receiver is online
         user_info = self.lookup_user(receiver_username)
+        self.current_chat = receiver_username
         print(f"Starting chat with {receiver_username}...")
         while True:
             message = input("You: ")
             if message.lower() == 'exit':
+                self.current_chat == None
                 return self.start_chat_session()
             # If receiver is online
             if user_info:
@@ -177,9 +128,45 @@ class Client:
             else:
                 self.store_sent_message(receiver_username, message, delivered=0)
 
+    def retrieve_past_messages(self, receiver_username, db_conn, cursor):
+        cursor.execute('''
+        SELECT SenderUsername, ReceiverUsername, MessageText, Timestamp 
+        FROM Messages 
+        WHERE SenderUsername = ? OR ReceiverUsername = ?
+        ORDER BY Timestamp ASC
+        ''', (receiver_username, receiver_username))
+        messages = cursor.fetchall()
+        if messages:
+            for msg in messages:
+                sender = msg[0]
+                if sender == self.username:
+                    sender = "You"
+                message_text = msg[2]
+                timestamp = msg[3]
+                print(f"{sender} ({timestamp}): {message_text}")
+        db_conn.commit()
+
+    def send_message(self, receiver_username, ip, port, message):
+        """Sends a message to another client. Stores the message if the recipient is offline."""
+        try:
+            # Format the message and sender information as a JSON string
+            message_data = json.dumps({
+                'sender_username': self.username,
+                'message_text': message
+            }).encode('utf-8')
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(5)  # Timeout to avoid hanging indefinitely
+                s.connect((ip, port))
+                s.sendall(message_data)
+            self.store_sent_message(receiver_username, message)
+
+        except Exception as e:
+            print(e)
+
     def store_sent_message(self, receiver_username, message, delivered=1):
         """Stores a message in the database. Delivered=0 is message pending to be sent."""
-        conn = sqlite3.connect('p2p_messaging_app.db')
+        conn = sqlite3.connect(self.db_file)
         cursor = conn.cursor()
         cursor.execute('''
         INSERT INTO Messages (SenderUsername, ReceiverUsername, MessageText, Delivered, Timestamp)
@@ -216,13 +203,19 @@ class Client:
                     print(f"Undelivered message to {receiver_username} sent successfully.")
                 except Exception as e:
                     print(f"Failed to send undelivered message to {receiver_username}: {e}")
+            else:
+                conn.commit()
 
 
 def run():
     server_ip = '127.0.0.1'  # Running locally
     server_port = 12345
     client_port = int(input("Introduce your port number: "))
+    if client_port == "exit":
+        return None
     username = str(input("Introduce your username: "))
+    if username == "exit":
+        return None
     client = Client(server_ip, server_port, username, client_port)
     client.register_in_server()
 
